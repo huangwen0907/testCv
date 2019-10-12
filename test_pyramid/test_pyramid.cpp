00217fd8b9f2d495fbb43a1d9e104ff1d399b161
Huang <huangwenfuture@sina.com>
2019-10-12 09:58:58 +0800

    test_pyramid: execute the ORB function to detect the features & descriptor about the image

--- test_pyramid/test_pyramid.cpp
+++ test_pyramid/test_pyramid.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 using namespace std;
 using namespace cv;
@@ -19,6 +21,24 @@ int main(int argv, char* argc)
 
     Mat lap = currImg;
     
+    // detect the feature and descriptor Through ORB
+    Ptr<ORB> orb = ORB::create ( 900, 1.2f, 8, 31, 0, 2, ORB::HARRIS_SCORE,31,20 );
+    Mat descriptor;
+    std::vector<KeyPoint> keypoints;
+
+
+
+    //check the keypoints from the img
+    orb->detect(currImg,keypoints);
+    // compute the descriptor from the img of current
+    orb->compute(currImg,keypoints,descriptor);
+
+        // plot the keypoints and descriptor
+    Mat outstanding;
+    drawKeypoints(currImg,keypoints,outstanding,Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+    // drawKeypoints(currImg,keypoints,outstanding);
+    imshow("keypoints",outstanding);
+
     Mat down_py, up_py;
     for(int l=0;l < levels; l++) {
         Mat down, up;
@@ -33,8 +53,8 @@ int main(int argv, char* argc)
 
     namedWindow("Laplace pyramid");
     imshow("Laplace pyramid",lap);
-    imshow("up_py",up_py);
-    imshow("down_py",down_py);
+    // imshow("up_py",up_py);
+    // imshow("down_py",down_py);
 
     waitKey(0);
     return 0;
