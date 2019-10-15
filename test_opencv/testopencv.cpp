6203b0d1f67d0fb6b4bacbb439a1d9083751df1b
Huang <huangwenfuture@sina.com>
2019-10-15 15:58:39 +0800

    testopencv: mat mask operation the image 1. do the sum of the nearest five points and minus the others 2: do the filter in the img

--- test_opencv/testopencv.cpp
+++ test_opencv/testopencv.cpp
@@ -31,6 +31,7 @@ int main(int argv, char* argc)
             cout << "No valid image, please get the correct address" << endl;
             return -1;
         }
+        namedWindow("fuck img");
         imshow("raw image",img);
 
         Mat grayImg;
@@ -46,6 +47,49 @@ int main(int argv, char* argc)
         // pyrDown the image (pyramid process)
         pyrDown(img,imgRect);
         imshow("Rectangle",imgRect);
+
+
+        // mat mask operation
+        // reference website:
+        // http://www.opencv.org.cn/opencvdoc/2.3.2/html/doc/tutorials/core/mat-mask-operations/mat-mask-operations.html#maskoperationsfilter
+        Mat sharPen;
+        const int nChannels = img.channels();
+        cout <<"Img channels:" << nChannels << endl;
+
+        //使用掩码矩阵的时候，我们先把矩阵中心的元素（上面的例子中是(0,0)位置的元素，也就是5
+        //对齐到要计算的目标像素上，再把邻域像素值和相应的矩阵元素值的乘积加起来。
+        // I(i,j) = 5*I(i,j) - {I(i-1,j)+I(i+1,j)+I(i,j-1)+I(i,j+1)} // sum the nearest 5 points value and then get the mean value
+        sharPen.create(img.size(),img.type());
+        for (int j =1;j< img.rows-1;++j) {
+            const uchar* pre = img.ptr<uchar>(j-1);
+            const uchar* cur = img.ptr<uchar>(j);
+            const uchar* next = img.ptr<uchar>(j+1);
+
+            uchar* output = sharPen.ptr<uchar>(j);
+
+            for (int i = nChannels;i<nChannels*(img.rows-1);++i) {
+                *output++ = saturate_cast<uchar>(5*cur[i]-cur[i-nChannels]-cur[i+nChannels]-pre[i]-next[i]);
+            }
+        }
+
+        //在图像的边界上，上面给出的公式会访问不存在的像素位置（比如(0,-1)）。
+        //因此我们的公式对边界点来说是未定义的。一种简单的解决方法，是不对这些边界点使用掩码，而直接把它们设为0：
+        sharPen.row(0).setTo(Scalar(0));
+        sharPen.row(sharPen.rows-1).setTo(Scalar(0));
+        sharPen.col(0).setTo(Scalar(0));
+        sharPen.col(sharPen.cols-1).setTo(Scalar(0));
+
+        imshow("sharpen",sharPen);
+
+        Mat kern = (Mat_<char>(3,3) <<  0, -1, 0,
+                                   -1,  5, -1,
+                                    0, -1,  0);
+        Mat sharpenK;
+        //滤波器在图像处理中的应用
+        filter2D(img,sharpenK,img.depth(),kern);
+
+        imshow("sharpenK",sharpenK);
+
     }
 
     {
